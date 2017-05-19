6865e784f253e4875c8ee2ef8dc86b2aaad530f2
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-05-19 14:57:04 -0400

    What have I done.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
 
+    $(".top-teeth").toggle("blind");
+    $(".bottom-teeth").toggle("blind", {"direction": "down"})
+
     $(".nose").on("click", function(){
-        $(".mouth").toggle("puff");
+        $(".hair").toggle("explode");
+        $(".nose").toggle("puff");
+        $(".small-circle").toggle("scale", {"percent": 1});
+        // $(".circle").toggle("fold");
+        $(".top").toggle("explode");
     })
 
     $(".circle").on("click", function(){
-        $(".small-circle").toggle("blind");
+        $(".small-circle").toggle("scale", {"percent": 142});
     })
 
     $(".mouth").on("click", function(){
-        $(".hair").toggle("explode");
-        $(".nose").toggle("puff");
-        $(".small-circle").toggle("slide");
-        // $(".circle").toggle("fold");
-        $(".top").toggle("explode");
         $(".top-teeth").toggle("blind");
         $(".bottom-teeth").toggle("blind", {"direction": "down"});
     })
-
 })
